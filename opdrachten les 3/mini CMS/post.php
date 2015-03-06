10216c10817fdebe4709f5fe3861642980104d51
NielsMeulders <niels.meulders@telenet.be>
2015-03-06 15:00:11 +0100

    added form validate and updated style

--- opdrachten les 3/mini CMS/post.php
+++ opdrachten les 3/mini CMS/post.php
@@ -2,20 +2,28 @@
 
     if(!empty($_POST))
     {
-        $title = $_POST['title'];
-        $article = $_POST['article'];
-        $date = date("d/m/y");
+        if (empty($_POST['title']) && empty($_POST['article']))
+        {
+            $feedback = "Fill in all the fields";
+        }
+        else
+        {
+            $title = $_POST['title'];
+            $article = $_POST['article'];
+            $date = date("d/m/y");
 
 
-        $conn = new PDO('mysql:host=localhost;dbname=phples', "root", "root");
-        // INSERT
-        $statement = $conn->prepare("INSERT INTO cms (title, date, article) VALUES ( :title, :date, :article )");
-        $statement->bindParam(':title', $title);
-        $statement->bindParam(':date', $date);
-        $statement->bindParam(':article', $article);
-        $statement->execute();
+            $conn = new PDO('mysql:host=localhost;dbname=phples', "root", "root");
+            // INSERT
+            $statement = $conn->prepare("INSERT INTO cms (title, date, article) VALUES ( :title, :date, :article )");
+            $statement->bindParam(':title', $title);
+            $statement->bindParam(':date', $date);
+            $statement->bindParam(':article', $article);
+            $statement->execute();
 
-        $feedback = "Your article is posted!";
+            $feedback = "Your article is posted!";
+        }
+
     }
 
 
@@ -36,7 +44,7 @@
 
         <a href="index.php" class="button">Go back!</a>
 
-        <?php if(isset($feedback)){echo '<div class="feedback">'.$feedback.'</div>';} ?>
+        <?php if(isset($feedback)){echo '<p id="feedback_form">'.$feedback.'</p>';} ?>
 
 
         <form action="" method="post">
