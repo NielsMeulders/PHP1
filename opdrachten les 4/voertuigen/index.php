ebc6073de3bccd8c633f13aef61c7e2a6ed6c28f
NielsMeulders <niels.meulders@telenet.be>
2015-03-10 14:18:29 +0100

    added toString function and testcase

--- opdrachten les 4/voertuigen/index.php
+++ opdrachten les 4/voertuigen/index.php
@@ -1,3 +1,25 @@
+<?php
+
+    try{
+        spl_autoload_register(function($class){
+            include_once("classes/".$class.".class.php");
+        });
+
+        $s = new sportwagen();
+        $s->Merk = "Renault";
+        $s->AantalPassagiers = 5;
+        $s->AantalDeuren = 5;
+
+        echo $s;
+    }
+    catch (Exception $e)
+    {
+        $error = $e->getMessage();
+    }
+
+?>
+
+
 <!doctype html>
 <html lang="nl">
 <head>
